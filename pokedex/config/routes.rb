b877ef56cb6ae446e9cb583c56981b71bcff2701
Suzi <lisuzanne94@gmail.com>
2021-10-22 15:28:36 -0400

    Finished phase 0

---
+++ pokedex/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  
+  namespace :api, defaults: {format: :json} do
+    resources :pokemon, only: [:create, :index, :show]
+  end
+
+  root to: 'static_pages#root'
+end
