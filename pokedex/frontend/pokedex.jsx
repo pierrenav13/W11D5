e4564b872ca7eebf5678a95f05dfac32d167cf6f
Suzi <lisuzanne94@gmail.com>
2021-10-22 16:58:20 -0400

    Working on phase 2

---
+++ pokedex/frontend/pokedex.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { fetchAllPokemon } from './util/api_util';
+import { receiveAllPokemon } from './actions/pokemon_actions';
+import configureStore from './store/store'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const rootEl = document.getElementById('root');
+    const store = configureStore();
+
+    window.fetchAllPokemon = fetchAllPokemon;
+    window.receiveAllPokemon = receiveAllPokemon;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+
+    ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+});
